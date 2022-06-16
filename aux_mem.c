82785b4a4f56108b414d18155415c71e6376aec7
Diadiva223 <101552499+Diadiva223@users.noreply.github.com>
2022-06-16 04:10:34 +0200

    trending `1

--- aux_mem.c
+++ aux_mem.c
@@ -78,14 +78,4 @@ char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	newptr = malloc(sizeof(char *) * new_size);
-	if (newptr == NULL)
-		return (NULL);
-
-	for (i = 0; i < old_size; i++)
-		newptr[i] = ptr[i];
-
-	free(ptr);
-
-	return (newptr);
-}
+	newptr = malloc(si
